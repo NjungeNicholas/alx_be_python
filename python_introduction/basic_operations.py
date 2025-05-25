841e368b7dd8aae8e5b1a236c989f17b42a26e72
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-05-25 17:47:05 +0300

    Added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Addition
+result_addition = number1 + number2
+# Subtraction
+result_subtraction = number1 - number2
+# Multiplication
+result_multiplication = number1 * number2
+print("Addition of", number1, "and", number2, "is:", result_addition)
+print("Subtraction of", number1, "and", number2, "is:", result_subtraction)
+print("Multiplication of", number1, "and", number2, "is:", result_multiplication)
