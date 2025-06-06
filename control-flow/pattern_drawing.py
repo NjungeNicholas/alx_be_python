45239d0cca74a1e772623e052f11c3489931de8e
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-06-06 20:47:42 +0300

    Added control-flow directory

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,8 @@
+size_of_pattern = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size_of_pattern:
+    for _ in range(size_of_pattern):
+        print("*", end="")
+    print()
+    row += 1
