18fb070b24cf3a3173fc3daf29838e70c3f94560
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-06-13 22:20:23 +0300

    refactored date handling in explore_datetime.py for clarity and consistency

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,19 +1,18 @@
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 # Function to display the current date and time
 def display_current_datetime():
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return current_date
 
 #Display current date and time
 print(f"Current Date and time: {display_current_datetime()}")
 
 # Function to add a specified number of days to the current date
-number_of_days = int(input("Enter the number of days to add: "))
+number_of_days = int(input("Enter the number of days to add to the current date: "))
 def calculate_future_date(days):
-    current_date = datetime.datetime.now()
-    new_date = current_date + datetime.timedelta(days=days)
-    return new_date
-print(f"Future date: {calculate_future_date(number_of_days).strftime('%Y-%m-%d')}")
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+    return future_date.strftime('%Y-%m-%d')
+print(f"Future date: {calculate_future_date(number_of_days)}")
