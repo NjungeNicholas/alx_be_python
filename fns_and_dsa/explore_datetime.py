01cb77ef83cad4b25dfdccf6911e6e0414ec1730
NjungeNicholas <J77-3874-2023@student.mksu.ac.ke>
2025-06-13 18:58:19 +0300

    changed the function name to calculate_future_date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -12,8 +12,8 @@ print(f"Current Date and time: {display_current_datetime()}")
 
 # Function to add a specified number of days to the current date
 number_of_days = int(input("Enter the number of days to add: "))
-def add_days_to_current_date(days):
+def calculate_future_date(days):
     current_date = datetime.datetime.now()
     new_date = current_date + datetime.timedelta(days=days)
     return new_date
-print(f"Future date: {add_days_to_current_date(number_of_days).strftime('%Y-%m-%d')}")
+print(f"Future date: {calculate_future_date(number_of_days).strftime('%Y-%m-%d')}")
